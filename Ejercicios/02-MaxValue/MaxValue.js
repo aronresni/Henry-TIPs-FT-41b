2533913f6fde4823189f71051cc24f6e58e08ec1
aronresni <aronresni@gmail.com>
2023-09-12 13:41:39 -0300

    maxvalue/succes

--- Ejercicios/02-MaxValue/MaxValue.js
+++ Ejercicios/02-MaxValue/MaxValue.js
@@ -1,5 +1,11 @@
+
 function MaxValue(shares) {
   // Your code here:
+  let min = Math.min(...shares);
+  let minIndex = shares.indexOf(min);
+  let newArr = shares.slice(minIndex);
+  let max = Math.max(...newArr);
+  return max - min;
 }
 
 module.exports = MaxValue;
