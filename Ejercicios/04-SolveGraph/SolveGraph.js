c0418ae354a0cf56c831b6450fbffbb5f5f2d8d5
aronresni <aronresni@gmail.com>
2023-09-14 14:08:07 -0300

    upload

--- Ejercicios/04-SolveGraph/SolveGraph.js
+++ Ejercicios/04-SolveGraph/SolveGraph.js
@@ -1,5 +1,12 @@
-function SolveGraph(graph, start, end) {
+function SolveGraph(graph, start, end, arr = []) {
   // Your code here:
+  if (arr.includes(start)) return false
+  arr.push(start);
+  for (const nodo of graph[start]) {
+    if (nodo === end) return true
+    if (SolveGraph(graph, nodo, end, arr)) return true
+  }
+  return false
 }
 
 module.exports = SolveGraph;
